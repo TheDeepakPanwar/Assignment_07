8b0b02969c771672d8c23e448e5d1631a0cc9bbc
Deepak Panwar <phd2301241002@iiti.ac.in>
2025-10-08 19:37:02 +0530

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,2 @@
+/* Write a program that allows users to enter five words (in the form of c-type strings) and concatenates these words in an alphabetical order 
+   with the first character of each word determining the order. For example, if there is a word “africa” and another word “europe”; africa would come before europe in the concatenated string.*/
